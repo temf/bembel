a5b52e9125bf0a05892524c5ed4721020a45c1d1
tex <mm@mm>
2021-08-19 22:59:39 +0200

    all tests pass, all examples seem to work. It remains to benchmark the new element tree. The best idea would be to add timings to the different routines to check scaling!

--- Bembel/src/ClusterTree/ElementTreeNode.hpp
+++ Bembel/src/ClusterTree/ElementTreeNode.hpp
@@ -17,18 +17,61 @@ namespace Bembel {
  */
 class ElementTreeNode {
  public:
+  //
+  //  \brief iterator struct for element tree nodes.
+  //         they may be used to iterator over the elements in a cluster.
+  //         to do so, however, the cluster must be set up by ElementTree
+  //         beforehand.
+  //
+  struct const_iterator {
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = ElementTreeNode;
+    using pointer = value_type *;
+    using reference = value_type &;
+
+    const_iterator(pointer ptr) : m_ptr(ptr) {}
+
+    reference operator*() const { return *m_ptr; }
+    const pointer operator->() const { return m_ptr; }
+
+    // Prefix increment
+    const_iterator &operator++() {
+      m_ptr = m_ptr->next_;
+      return *this;
+    }
+
+    // Postfix increment
+    const_iterator operator++(int) {
+      const_iterator tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+
+    friend bool operator==(const const_iterator &a, const const_iterator &b) {
+      return a.m_ptr == b.m_ptr;
+    };
+    friend bool operator!=(const const_iterator &a, const const_iterator &b) {
+      return a.m_ptr != b.m_ptr;
+    };
+
+   private:
+    pointer m_ptr;
+  };
   //////////////////////////////////////////////////////////////////////////////
   /// constructors
   //////////////////////////////////////////////////////////////////////////////
-  ElementTreeNode()
-      : id_(-1),
+  ElementTreeNode() noexcept
+      : prev_(nullptr),
+        next_(nullptr),
+        id_(-1),
         level_(-1),
         patch_(-1),
         radius_(std::numeric_limits<double>::infinity()) {
     midpoint_ << 0., 0., 0.;
     llc_ << 0., 0.;
   }
-  ElementTreeNode(ElementTreeNode &&other) {
+  ElementTreeNode(ElementTreeNode &&other) noexcept {
     midpoint_.swap(other.midpoint_);
     llc_.swap(other.llc_);
     vertices_ = std::move(other.vertices_);
@@ -39,6 +82,8 @@ class ElementTreeNode {
     adjcents_ = std::move(other.adjcents_);
   }
   ElementTreeNode(const ElementTreeNode &other) = delete;
+  ElementTreeNode &operator=(const ElementTreeNode &other) = delete;
+  ElementTreeNode &operator=(ElementTreeNode &&other) = delete;
   //////////////////////////////////////////////////////////////////////////////
   /// methods
   //////////////////////////////////////////////////////////////////////////////
@@ -80,36 +125,46 @@ class ElementTreeNode {
   //////////////////////////////////////////////////////////////////////////////
   int get_level() const { return level_; }
   //////////////////////////////////////////////////////////////////////////////
-  const ElementTreeNode *front() const {
+  const ElementTreeNode &front() const {
     const ElementTreeNode *ptr = this;
     while (ptr->sons_.size()) ptr = std::addressof(ptr->sons_.front());
-    return ptr;
+    return *ptr;
   }
   //////////////////////////////////////////////////////////////////////////////
-  const ElementTreeNode *back() const {
+  const ElementTreeNode &back() const {
     const ElementTreeNode *ptr = this;
     while (ptr->sons_.size()) ptr = std::addressof(ptr->sons_.back());
-    return ptr;
-  }
-  const ElementTreeNode *end() const {
-    const ElementTreeNode *ptr = this;
-    while (ptr->sons_.size()) ptr = std::addressof(ptr->sons_.back());
-    return ptr->next_;
+    return *ptr;
   }
   //////////////////////////////////////////////////////////////////////////////
+  const_iterator cbegin() const {
+    const ElementTreeNode &bla = this->front();
+    return const_iterator(const_cast<ElementTreeNode *>(std::addressof(bla)));
+  }
+  //////////////////////////////////////////////////////////////////////////////
+  const_iterator cend() const {
+    const ElementTreeNode &bla = this->back();
+    return const_iterator(const_cast<ElementTreeNode *>(bla.next_));
+  }
+  //////////////////////////////////////////////////////////////////////////////
+  const_iterator begin() const { return cbegin(); }
+  //////////////////////////////////////////////////////////////////////////////
+  const_iterator end() const {
+    return cend();
+  }  //////////////////////////////////////////////////////////////////////////////
   /// member variables
   //////////////////////////////////////////////////////////////////////////////
   std::vector<ElementTreeNode> sons_;        /// children
   std::vector<ElementTreeNode *> adjcents_;  /// neighbouring elements indices
+  std::vector<int> vertices_;                /// indices of the vertices
+  Eigen::Vector3d midpoint_;                 /// midpoint of the element
+  Eigen::Vector2d llc_;                      /// lower left corner on [0,1]^2
   ElementTreeNode *prev_;
   ElementTreeNode *next_;
-  Eigen::Vector3d midpoint_;   /// midpoint of the element
-  Eigen::Vector2d llc_;        /// lower left corner on [0,1]^2
-  std::vector<int> vertices_;  /// indices of the vertices
-  double radius_;              /// radius of the element
-  int id_;                     /// element id with respect to the level
-  int level_;                  /// level of the element
-  int patch_;                  /// patch of the element
+  double radius_;  /// radius of the element
+  int id_;         /// element id with respect to the level
+  int level_;      /// level of the element
+  int patch_;      /// patch of the element
 };
 }  // namespace Bembel
 #endif
