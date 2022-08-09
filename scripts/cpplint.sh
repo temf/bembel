f4b7585159e4a44fda88779b3f524db6a840eefc
Maximilian Nolte <maximilian.nolte@gmail.com>
2022-08-09 10:44:07 +0200

    Removes printing of the errors to file

--- scripts/cpplint.sh
+++ scripts/cpplint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd Bembel;
-cpplint --quiet --extensions=hpp,cpp --root=.. --recursive ./src 2>> ../scripts/cpplint_output.txt;
-cat ../scripts/cpplint_output.txt
+cpplint --quiet --extensions=hpp,cpp --root=.. --recursive ./src
