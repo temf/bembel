d890d013956ac05f2b865e7832b8e4547a0efa16
Maximilian Nolte <maximilian.nolte@gmail.com>
2022-08-10 10:26:24 +0200

    Removes double header

--- Bembel/src/LinearOperator/Dummy/DummyOperator.hpp
+++ Bembel/src/LinearOperator/Dummy/DummyOperator.hpp
@@ -5,15 +5,6 @@
 // source code is subject to the GNU General Public License version 3 and
 // provided WITHOUT ANY WARRANTY, see <http://www.bembel.eu> for further
 // information.
-//
-// This file is part of Bembel, the higher order C++ boundary element library.
-// It was written as part of a cooperation of J. Doelz, H. Harbrecht, S. Kurz,
-// M. Multerer, S. Schoeps, and F. Wolf at Technische Universitaet Darmstadt,
-// Universitaet Basel, and Universita della Svizzera italiana, Lugano. This
-// source code is subject to the GNU General Public License version 3 and
-// provided WITHOUT ANY WARRANTY, see <http://www.bembel.eu> for further
-// information.
-//
 #ifndef BEMBEL_SRC_LINEAROPERATOR_DUMMY_DUMMYOPERATOR_HPP_
 #define BEMBEL_SRC_LINEAROPERATOR_DUMMY_DUMMYOPERATOR_HPP_
 
