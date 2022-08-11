145e884c6cb35158aec9eb93fd82d37bda462a38
Maximilian Nolte <maximilian.nolte@gmail.com>
2022-08-11 12:11:16 +0200

    Addes copyright to file header and some other header fixes

--- examples/Quadrature.cpp
+++ examples/Quadrature.cpp
@@ -1,3 +1,14 @@
+// This file is part of Bembel, the higher order C++ boundary element library.
+//
+// Copyright (C) 2022 see <http://www.bembel.eu>
+//
+// It was written as part of a cooperation of J. Doelz, H. Harbrecht, S. Kurz,
+// M. Multerer, S. Schoeps, and F. Wolf at Technische Universitaet Darmstadt,
+// Universitaet Basel, and Universita della Svizzera italiana, Lugano. This
+// source code is subject to the GNU General Public License version 3 and
+// provided WITHOUT ANY WARRANTY, see <http://www.bembel.eu> for further
+// information.
+
 #include <Bembel/Quadrature>
 #include <iostream>
 
