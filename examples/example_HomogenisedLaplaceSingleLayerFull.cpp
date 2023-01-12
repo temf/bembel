990650210046353e03d1d599c06f5595e5e4da68
remo <remo.vonrickenbach@unibas.ch>
2023-01-12 11:17:56 +0100

    added the cpplint conventions to the examples and test

--- examples/example_HomogenisedLaplaceSingleLayerFull.cpp
+++ examples/example_HomogenisedLaplaceSingleLayerFull.cpp
@@ -5,8 +5,6 @@
  *      Author: ricrem00
  */
 
-#include <iostream>
-
 #include <Eigen/Dense>
 
 #include <Bembel/AnsatzSpace>
@@ -15,78 +13,86 @@
 #include <Bembel/HomogenisedLaplace>
 #include <Bembel/LinearForm>
 
-#include "Data.hpp"
-#include "Error.hpp"
-#include "Grids.hpp"
+#include <iostream>
+
+#include "./Data.hpp"
+#include "./Error.hpp"
+#include "./Grids.hpp"
 
 int main() {
-	using namespace Bembel;
-	using namespace Eigen;
+  using Bembel::HomogenisedLaplaceSingleLayerOperator;
+  using Bembel::HomogenisedLaplaceSingleLayerPotential;
 
-	// Load geometry from file "sphere.dat", which must be placed in the same
-	// directory as the executable
-	Geometry geometry("../geo/cube_small.dat");
+  using Eigen::Vector3d;
+  using Eigen::VectorXd;
+  using Eigen::MatrixXd;
 
-	// Define evaluation points for potential field, a tensor product grid of
-	// 10*10*10 points in [0, 0.25]^3
-	MatrixXd gridpoints = Util::makeTensorProductGrid(
-			VectorXd::LinSpaced(10, 0.05, 0.20), VectorXd::LinSpaced(10, 0.05, 0.20),
-			VectorXd::LinSpaced(10, 0.05, 0.20));
+  // Load geometry from file "sphere.dat", which must be placed in the same
+  // directory as the executable
+  Bembel::Geometry geometry("../geo/cube_small.dat");
 
-	// Define analytical solution using lambda function, in this case a harmonic
-	// function, see Data.hpp
-	std::function<double(Vector3d)> fun = [](Vector3d in) {
-		return Data::HarmonicFunction(in);
-	};
+  // Define evaluation points for potential field, a tensor product grid of
+  // 10*10*10 points in [0, 0.25]^3
+  MatrixXd gridpoints = Bembel::Util::makeTensorProductGrid(
+      VectorXd::LinSpaced(10, 0.05, 0.20), VectorXd::LinSpaced(10, 0.05, 0.20),
+      VectorXd::LinSpaced(10, 0.05, 0.20));
 
-	// Define the precision of the fundamental solution of the potential and the operator
-	double precision = 1e-7;
-	HomogenisedLaplaceSingleLayerOperator::setPrecision(precision);
+  // Define analytical solution using lambda function, in this case a harmonic
+  // function, see Data.hpp
+  std::function<double(Vector3d)> fun = [](Vector3d in) {
+    return Bembel::Data::HarmonicFunction(in);
+  };
 
-	std::cout << "\n============================================================="
-			"==========\n";
-	// Iterate over polynomial degree.
-	for (unsigned int polynomial_degree : {0, 1, 2, 3}) {
-		// Iterate over refinement levels
-		for (unsigned int refinement_level : {0, 1, 2, 3}) {
-			std::cout << "Degree " << polynomial_degree << " Level "
-					<< refinement_level << "\t\t";
-			// Build ansatz space
-			AnsatzSpace<HomogenisedLaplaceSingleLayerOperator> ansatz_space(
-					geometry, refinement_level, polynomial_degree);
+  // Define the precision of the fundamental solution of the
+  // potential and the operator
+  double precision = 1e-7;
+  HomogenisedLaplaceSingleLayerOperator::setPrecision(precision);
 
-			// Set up load vector
-			DiscreteLinearForm<DirichletTrace<double>, HomogenisedLaplaceSingleLayerOperator>
-			disc_lf(ansatz_space);
-			disc_lf.get_linear_form().set_function(fun);
-			disc_lf.compute();
+  std::cout << "\n============================================================="
+      "==========\n";
+  // Iterate over polynomial degree.
+  for (unsigned int polynomial_degree : { 0, 1, 2, 3 }) {
+    // Iterate over refinement levels
+    for (unsigned int refinement_level : { 0, 1, 2, 3 }) {
+      std::cout << "Degree " << polynomial_degree << " Level "
+          << refinement_level << "\t\t";
+      // Build ansatz space
+      Bembel::AnsatzSpace<HomogenisedLaplaceSingleLayerOperator> ansatz_space(
+          geometry, refinement_level, polynomial_degree);
 
-			// Set up and compute discrete operator
-			DiscreteOperator<MatrixXd, HomogenisedLaplaceSingleLayerOperator> disc_op(
-					ansatz_space);
-			disc_op.compute();
+      // Set up load vector
+      Bembel::DiscreteLinearForm<Bembel::DirichletTrace<double>,
+          HomogenisedLaplaceSingleLayerOperator> disc_lf(ansatz_space);
+      disc_lf.get_linear_form().set_function(fun);
+      disc_lf.compute();
 
-			// solve system
-			LLT<MatrixXd> llt;
-			llt.compute(disc_op.get_discrete_operator());
-			VectorXd rho = llt.solve(disc_lf.get_discrete_linear_form());
+      // Set up and compute discrete operator
+      Bembel::DiscreteOperator<MatrixXd, HomogenisedLaplaceSingleLayerOperator>
+        disc_op(ansatz_space);
+      disc_op.compute();
 
-			// evaluate potential
-			DiscretePotential<HomogenisedLaplaceSingleLayerPotential<HomogenisedLaplaceSingleLayerOperator>,
-			HomogenisedLaplaceSingleLayerOperator>
-			disc_pot(ansatz_space);
-			disc_pot.set_cauchy_data(rho);
-			VectorXd pot = disc_pot.evaluate(gridpoints);
+      // solve system
+      Eigen::LLT<MatrixXd> llt;
+      llt.compute(disc_op.get_discrete_operator());
+      VectorXd rho = llt.solve(disc_lf.get_discrete_linear_form());
 
-			// print error
-			std::cout << maxPointwiseError<double>(pot, gridpoints, fun) << std::endl;
-		}
-		std::cout << std::endl;
-	}
-	std::cout << "============================================================="
-			"=========="
-			<< std::endl;
+      // evaluate potential
+      Bembel::DiscretePotential<
+          HomogenisedLaplaceSingleLayerPotential<
+              HomogenisedLaplaceSingleLayerOperator>,
+          HomogenisedLaplaceSingleLayerOperator> disc_pot(ansatz_space);
+      disc_pot.set_cauchy_data(rho);
+      VectorXd pot = disc_pot.evaluate(gridpoints);
 
-	return 0;
+      // print error
+      std::cout << Bembel::maxPointwiseError<double>(pot, gridpoints, fun)
+          << std::endl;
+    }
+    std::cout << std::endl;
+  }
+  std::cout << "============================================================="
+      "==========" << std::endl;
+
+  return 0;
 }
 
--- tests/test_HomogenisedCoefficients.cpp
+++ tests/test_HomogenisedCoefficients.cpp
@@ -5,109 +5,116 @@
  *      Author: ricrem00
  */
 
+#include <Bembel/HomogenisedLaplace>
+#include <Eigen/Dense>
 
 #include <iostream>
 #include <functional>
 
-#include <math.h>
+inline double k_per(Eigen::Vector3d in, Eigen::VectorXd coeffs,
+    unsigned int deg);
 
-#include <Bembel/HomogenisedLaplace>
-#include <Eigen/Dense>
-
-inline double k_per(Eigen:: Vector3d in, Eigen::VectorXd coeffs, unsigned int deg);
-
-inline Eigen::Vector3d Dk_per(Eigen:: Vector3d in, Eigen::VectorXd coeffs, unsigned int deg);
+inline Eigen::Vector3d Dk_per(Eigen::Vector3d in, Eigen::VectorXd coeffs,
+    unsigned int deg);
 
 Eigen::Matrix<double, 2, Eigen::Dynamic> tensorise(Eigen::ArrayXd xs);
 
 int main() {
+  using Eigen::VectorXd;
+  using Eigen::Vector3d;
+  using Eigen::ArrayXd;
+  using Eigen::MatrixXd;
 
-	using namespace Bembel;
-	using namespace Eigen;
+  double precision = 1e-7;
+  unsigned int deg = Bembel::getDegree(precision);
 
-	double precision = 1e-12;
-	unsigned int deg = getDegree(precision);
+  VectorXd cs = Bembel::getCoefficients(precision);
 
-	VectorXd cs = getCoefficients(precision);
+  unsigned int Npoints = 101;
 
-	unsigned int Npoints = 101;
+  ArrayXd xs = ArrayXd::LinSpaced(Npoints, -0.5, 0.5);
+  MatrixXd ys = tensorise(xs);
 
-	ArrayXd xs = ArrayXd::LinSpaced(Npoints, -0.5, 0.5);
-	MatrixXd ys = tensorise(xs);
+  double err = 0.0;
 
-	double err = 0.0;
+  Vector3d ex(1.0, 0.0, 0.0);
+  Vector3d ey(0.0, 1.0, 0.0);
+  Vector3d ez(0.0, 0.0, 1.0);
 
-	Vector3d ex(1.0, 0.0, 0.0);
-	Vector3d ey(0.0, 1.0, 0.0);
-	Vector3d ez(0.0, 0.0, 1.0);
+  std::function<double(Vector3d)> u = [cs, deg](Vector3d in) {
+    return k_per(in, cs, deg);
+  };
+  std::function<double(Vector3d, unsigned int)> Du = [cs, deg](Vector3d in,
+      unsigned int d) {
+    return Dk_per(in, cs, deg)(d);
+  };
 
-	std::function<double(Vector3d)> u = [cs, deg](Vector3d in) 									{return k_per(in, cs, deg);};
-	std::function<double(Vector3d, unsigned int)> Du = [cs, deg](Vector3d in, unsigned int d)	{return Dk_per(in, cs, deg)(d);};
+  unsigned int k;
+  Vector3d v;
+  for (k = 0; k < Npoints * Npoints; k++) {
+    v = Vector3d(-0.5, ys(0, k), ys(1, k));
+    err += abs(k_per(v, cs, deg) - k_per(v + ex, cs, deg));
+    err += abs(Du(v, 0) - Du(v + ex, 0));
 
+    v = Vector3d(ys(0, k), -0.5, ys(1, k));
+    err += abs(u(v) - u(v + ey));
+    err += abs(Du(v, 1) - Du(v + ey, 1));
 
-	unsigned int k;
-	Vector3d v;
-	for(k = 0; k < Npoints*Npoints; k++) {
-		v = Vector3d(-0.5, ys(0, k), ys(1, k));
-		err += fabs(k_per(v, cs, deg) - k_per(v + ex, cs, deg));
-		err += fabs(Du(v, 0) - Du(v + ex, 0));
+    v = Vector3d(ys(0, k), ys(1, k), -0.5);
+    err += abs(u(v) - u(v + ez));
+    err += abs(Du(v, 2) - Du(v + ez, 2));
+  }
 
-		v = Vector3d(ys(0, k), -0.5, ys(1, k));
-		err += fabs(u(v) - u(v + ey));
-		err += fabs(Du(v, 1) - Du(v + ey, 1));
+  err /= (6 * Npoints * Npoints);
 
-		v = Vector3d(ys(0, k), ys(1, k), -0.5);
-		err += fabs(u(v) - u(v + ez));
-		err += fabs(Du(v, 2) - Du(v + ez, 2));
-	}
+  std::cout << "Average Pointwise Error is\t" << err << std::endl;
 
-	err /= (6*Npoints*Npoints);
+  /* test the gradient with the differential quotient */
 
-	std::cout << "Average Pointwise Error is\t" << err << std::endl;
+  double h = 1e-8;
+  Vector3d p(0.1, 0.2, -0.3);
+  Vector3d dir = Vector3d::Random(3).normalized();
+  Vector3d t = p + h * dir;
+  double dq = (k_per(t, cs, deg) - k_per(p, cs, deg)) / h;
+  double gr = Dk_per(p, cs, deg).dot(dir);
 
-	/* test the gradient with the differential quotient */
+  std::cout << "Error in difference quotient is\t" << abs(dq - gr) << std::endl;
 
-	double h = 1e-8;
-	Vector3d p(0.1, 0.2, -0.3);
-	Vector3d dir = Vector3d::Random(3).normalized();
-	Vector3d t = p + h*dir;
-	double dq = (k_per(t, cs, deg) - k_per(p, cs, deg))/h;
-	double gr = Dk_per(p, cs, deg).dot(dir);
-
-	std::cout << "Error in difference quotient is\t" << abs(dq - gr) << std::endl;
-
-	return 0;
+  return 0;
 }
 
-inline double k_per(Eigen::Vector3d in, Eigen::VectorXd coeffs, unsigned int deg) {
+inline double k_per(Eigen::Vector3d in, Eigen::VectorXd coeffs,
+    unsigned int deg) {
 
-	return Bembel::k_mod(in) + Bembel::evaluate_solid_sphericals(in, coeffs, deg, false);
+  return Bembel::k_mod(in)
+      + Bembel::evaluate_solid_sphericals(in, coeffs, deg, false);
 }
 
-inline Eigen::Vector3d Dk_per(Eigen::Vector3d in, Eigen::VectorXd coeffs, unsigned int deg) {
+inline Eigen::Vector3d Dk_per(Eigen::Vector3d in, Eigen::VectorXd coeffs,
+    unsigned int deg) {
 
-	if(deg == 0) {return Bembel::Dk_mod(in);}
+  if (deg == 0) {
+    return Bembel::Dk_mod(in);
+  }
 
-	Eigen::Vector3d res = Bembel::Dk_mod(in);
-	res += (in/in.norm())*Bembel::evaluate_solid_sphericals(in, coeffs, deg, true);
-	res += Bembel::functionalMatrix(in)*Bembel::evaluate_dsolid_sphericals(in, coeffs, deg);
+  Eigen::Vector3d res = Bembel::Dk_mod(in);
+  res += (in / in.norm())
+      * Bembel::evaluate_solid_sphericals(in, coeffs, deg, true);
+  res += Bembel::functionalMatrix(in)
+      * Bembel::evaluate_dsolid_sphericals(in, coeffs, deg);
 
-	return res;
+  return res;
 }
 
 Eigen::Matrix<double, 2, Eigen::Dynamic> tensorise(Eigen::ArrayXd xs) {
+  unsigned int len = xs.rows();
+  Eigen::MatrixXd ys(2, len * len);
 
-	unsigned int len = xs.rows();
+  unsigned int k;
+  for (k = 0; k < len; k++) {
+    ys.block(0, k * len, 1, len) = xs(k) * Eigen::MatrixXd::Ones(1, len);
+    ys.block(1, k * len, 1, len) = xs.transpose();
+  }
 
-
-	Eigen::MatrixXd ys(2, len*len);
-
-	unsigned int k;
-	for(k = 0; k < len; k++) {
-		ys.block(0, k*len, 1, len) = xs(k)*Eigen::MatrixXd::Ones(1, len);
-		ys.block(1, k*len, 1, len) = xs.transpose();
-	}
-
-	return ys;
-
+  return ys;
 }
