3c6ddca038d098a4356c098c663e8e61ee5ba9b6
Jürgen Dölz <doelz@ins.uni-bonn.de>
2023-01-30 09:44:02 +0100

    Removed issues with style guide

--- Bembel/src/LinearForm/NeumannTrace.hpp
+++ Bembel/src/LinearForm/NeumannTrace.hpp
@@ -1,12 +1,15 @@
 // This file is part of Bembel, the higher order C++ boundary element library.
+//
+// Copyright (C) 2022 see <http://www.bembel.eu>
+//
 // It was written as part of a cooperation of J. Doelz, H. Harbrecht, S. Kurz,
 // M. Multerer, S. Schoeps, and F. Wolf at Technische Universitaet Darmstadt,
 // Universitaet Basel, and Universita della Svizzera italiana, Lugano. This
 // source code is subject to the GNU General Public License version 3 and
 // provided WITHOUT ANY WARRANTY, see <http://www.bembel.eu> for further
 // information.
-#ifndef BEMBEL_LINEARFORM_NEUMANNTRACE_H_
-#define BEMBEL_LINEARFORM_NEUMANNTRACE_H_
+#ifndef BEMBEL_SRC_LINEARFORM_NEUMANNTRACE_HPP_
+#define BEMBEL_SRC_LINEARFORM_NEUMANNTRACE_HPP_
 
 namespace Bembel {
 
@@ -63,11 +66,11 @@ class NeumannTrace : public LinearFormBase<NeumannTrace<Scalar>, Scalar> {
     super_space.addScaledBasis(intval, integrand, s);
 
     return;
-  };
+  }
 
  private:
   std::function<Eigen::Matrix<Scalar, 3, 1>(Eigen::Vector3d)> function_;
 };
 }  // namespace Bembel
 
-#endif
+#endif  // BEMBEL_SRC_LINEARFORM_NEUMANNTRACE_HPP_
