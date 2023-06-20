60dd6a49b87a09eb8965a00804ba84807392cb3a
remo <remo.vonrickenbach@unibas.ch>
2023-06-20 15:31:46 +0200

    included the headers into the Operator and Potential

--- Bembel/src/HomogenisedLaplace/Coefficients.hpp
+++ Bembel/src/HomogenisedLaplace/Coefficients.hpp
@@ -1,9 +1,13 @@
-/*
- * Coefficients.hpp
- *
- *  Created on: 3 Jan 2023
- *      Author: ricrem00
- */
+// This file is part of Bembel, the higher order C++ boundary element library.
+//
+// Copyright (C) 2022 see <http://www.bembel.eu>
+//
+// It was written as part of a cooperation of J. Doelz, H. Harbrecht, S. Kurz,
+// M. Multerer, S. Schoeps, and F. Wolf at Technische Universitaet Darmstadt,
+// Universitaet Basel, and Universita della Svizzera italiana, Lugano. This
+// source code is subject to the GNU General Public License version 3 and
+// provided WITHOUT ANY WARRANTY, see <http://www.bembel.eu> for further
+// information.
 
 #ifndef BEMBEL_SRC_HOMOGENISEDLAPLACE_COEFFICIENTS_HPP_
 #define BEMBEL_SRC_HOMOGENISEDLAPLACE_COEFFICIENTS_HPP_
@@ -383,4 +387,4 @@ double calculateFirstCoefficient(Eigen::VectorXd cs, unsigned int deg,
 
 } /* namespace Bembel */
 
-#endif /* BEMBEL_SRC_HOMOGENISEDLAPLACE_COEFFICIENTS_HPP_ */
+#endif  // BEMBEL_SRC_HOMOGENISEDLAPLACE_COEFFICIENTS_HPP_
--- Bembel/src/HomogenisedLaplace/SingleLayerPotential.hpp
+++ Bembel/src/HomogenisedLaplace/SingleLayerPotential.hpp
@@ -1,9 +1,13 @@
-/*
- * SingleLayerPotential.hpp
- *
- *  Created on: 9 Jan 2023
- *      Author: ricrem00
- */
+// This file is part of Bembel, the higher order C++ boundary element library.
+//
+// Copyright (C) 2022 see <http://www.bembel.eu>
+//
+// It was written as part of a cooperation of J. Doelz, H. Harbrecht, S. Kurz,
+// M. Multerer, S. Schoeps, and F. Wolf at Technische Universitaet Darmstadt,
+// Universitaet Basel, and Universita della Svizzera italiana, Lugano. This
+// source code is subject to the GNU General Public License version 3 and
+// provided WITHOUT ANY WARRANTY, see <http://www.bembel.eu> for further
+// information.
 
 #ifndef BEMBEL_SRC_HOMOGENISEDLAPLACE_SINGLELAYERPOTENTIAL_HPP_
 #define BEMBEL_SRC_HOMOGENISEDLAPLACE_SINGLELAYERPOTENTIAL_HPP_
