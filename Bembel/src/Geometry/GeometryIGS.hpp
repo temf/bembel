d79f661cde2a7acb52491a3a6112661781659d86
Maximilian Nolte <38978321+mx-nlte@users.noreply.github.com>
2024-01-26 16:43:03 +0100

    Print Header and Global Section

--- Bembel/src/Geometry/GeometryIGS.hpp
+++ Bembel/src/Geometry/GeometryIGS.hpp
@@ -139,5 +139,118 @@ std::vector<Patch> LoadGeometryFileIGS(const std::string &file_name) noexcept {
   return out;
 }
 
+void writeIGSHeader(std::string file_name) {
+  std::ofstream file;
+  file.open(file_name);
+
+  file << "\r\n";
+  file << "IGES obtained from Bembel.\r\n";
+  file << "See <http://www.bembel.eu>\r\n";
+  file << "\r\n";
+}
+
+std::vector<std::string> makeSection(std::vector<std::string> data,
+                                     const int linewidth = 72) {
+  std::vector<std::string> out;
+  out.reserve(data.size());
+
+  std::string line = "";
+  const int number_of_entries = data.size();
+  for (auto i = 0; i < number_of_entries; ++i) {
+    std::string item = data[i];
+    if (i < number_of_entries - 1)
+      item += ",";
+    else
+      item += ";";
+
+    const int line_length = line.length() + item.length();
+    if (line_length > linewidth) {
+      out.push_back(line);
+      line = "";
+    } else {
+      line += item;
+    }
+  }
+  out.push_back(line);
+  return out;
+}
+void writeGlobalSection(std::string file_name) {
+  std::vector<std::string> out(24);
+  std::time_t now = std::time(nullptr);
+
+  std::tm* localTime = std::localtime(&now);
+  char dateString[11];  // "YYYY-MM-DD\0"
+  std::strftime(dateString, sizeof(dateString), "%Y-%m-%d", localTime);
+  // Parameter Deliminator Character
+  out[0] = "1H,";
+  // Record Delimiter Character
+  out[1] = "1H;";
+  // Product ID from Sender
+  out[2] = "Bembel";
+  // File Name
+  out[3] = file_name;
+  // System ID
+  out[4] = "Bembel";
+  // Pre-processor Version
+  out[5] = "writeIGSFile";
+  // Number of Bits for Integers
+  out[6] = "32";
+  // Single Precision Magnitude
+  out[7] = "75";
+  // Single Precision Significance
+  out[8] = "6";
+  // Double Precision Magnitude
+  out[9] = "75";
+  // Double Precision Significance
+  out[10] = "15";
+  // Product ID for Receiver
+  out[11] = "Nurbs from Bembel";
+  // Model Space Scale
+  out[12] = "1.0";
+  // Unit Flag (6 = metres)
+  out[13] = "6";
+  // Units  (metres = "M")
+  out[14] = "M";
+  // Maximum Number of Line Weights
+  out[15] = "1000";
+  // Size of Maximum Line Width
+  out[16] = "1.0";
+  // Date and Time of file generation
+  out[17] = dateString;
+  // Minimum User-intended Resolution
+  out[18] = "0.000001";
+  // Approximate Maximum Coordinate
+  out[19] = "10000.0";
+  // Name of Author
+  out[20] = "Maximilian Nolte";
+  // Author's Organization
+  out[21] = "CEM - TU Darmstadt";
+  // IGES Version Number (3 = IGES version 2.0)
+  out[22] = "3";
+  // Drafting Standard Code (0 = no standard)
+  out[23] = "0";
+
+  std::vector<std::string> section = makeSection(out);
+
+  std::ofstream file(file_name, std::ios::app);
+  for (auto it = section.begin(); it != section.end(); ++it) {
+    const int index = std::distance(section.begin(), it);
+    file << std::left << std::setw(72) << *it << "G" << std::setw(7) << index
+         << "\n";
+  }
+}
+
+/**
+ * \ingroup Geometry
+ * \brief Writes Geometry into an IGES file format.
+ *
+ * \param name path/filename for the file to be written.
+ */
+
+void writeIGSFile(std::vector<Patch> geometry, std::string file_name) {
+  writeIGSHeader(file_name);
+  writeGlobalSection(file_name);
+  return;
+}
 }  // namespace Bembel
 #endif  // BEMBEL_SRC_GEOMETRY_GEOMETRYIGS_HPP_
