8edaffd4341e3f7524810a3c9ed2e6d2bd27855d
Maximilian Nolte <38978321+mx-nlte@users.noreply.github.com>
2024-02-14 16:53:45 +0100

    Revert Changing MaxP and Use Constant in DeBoor.hpp

--- Bembel/src/util/Constants.hpp
+++ Bembel/src/util/Constants.hpp
@@ -19,7 +19,7 @@ namespace Constants {
 ////////////////////////////////////////////////////////////////////////////////
 constexpr double generic_tolerance = 1e-6;
 // some not further specified constant
-constexpr int MaxP = 18;
+constexpr int MaxP = 20;
 
 // some not further specified constant
 constexpr int maximum_quadrature_degree = 50;
