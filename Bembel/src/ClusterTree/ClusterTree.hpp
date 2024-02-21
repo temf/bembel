8d993cee60c33a0e46f692b03f151f3990ade6bc
Maximilian Nolte <38978321+mx-nlte@users.noreply.github.com>
2024-02-21 08:11:00 +0100

    Add Documentation of ClusterTree.hpp

--- Bembel/src/ClusterTree/ClusterTree.hpp
+++ Bembel/src/ClusterTree/ClusterTree.hpp
@@ -14,8 +14,8 @@
 namespace Bembel {
 
 /**
- *  \ingroup ClusterTree
- *  \brief The ClusterTree class introduces an element structure on a Geometry
+ * \ingroup ClusterTree
+ * \brief The ClusterTree class introduces an element structure on a Geometry
  * object. Note that we do not introduce a mesh in the classical sense, but only
  * introduce a system of local coordinates via an ElementTree.
  */
@@ -23,18 +23,64 @@ class ClusterTree {
  public:
   // we declare functionality which has not been implemented (yet)
   // to be private
+  /**
+   * \brief Copy constructor (deleted).
+   *
+   * \param other The ClusterTree instance to copy from.
+   */
   ClusterTree(const ClusterTree& other) = delete;
+
+  /**
+   * \brief Move constructor (deleted).
+   *
+   * \param other The ClusterTree instance to move from.
+   */
   ClusterTree(ClusterTree&& other) = delete;
+
+  /**
+   * \brief Copy assignment operator (deleted).
+   *
+   * \param other The ClusterTree instance to copy from.
+   * \return Reference to this ClusterTree instance.
+   */
   ClusterTree& operator=(const ClusterTree& other) = delete;
+
+  /**
+   * \brief Move assignment operator (deleted).
+   *
+   * \param other The ClusterTree instance to move from.
+   * \return Reference to this ClusterTree instance.
+   */
   ClusterTree& operator=(ClusterTree&& other) = delete;
   //////////////////////////////////////////////////////////////////////////////
   /// constructors
   //////////////////////////////////////////////////////////////////////////////
+  /**
+   * \brief Default constructor.
+   */
   ClusterTree() {}
+  /**
+   * \brief Constructs a ClusterTree object for a given refinement level.
+   *
+   * Initializes a new instance of the ClusterTree class with the given geometry
+   * and refinement level.
+   *
+   * \param geom The geometry object to construct a cluster tree on.
+   * \param M refinement level of the ElementTree.
+   */
   ClusterTree(const Geometry& geom, int M) { init_ClusterTree(geom, M); }
   //////////////////////////////////////////////////////////////////////////////
   /// init
   //////////////////////////////////////////////////////////////////////////////
+  /**
+   * \brief Initializes a ClusterTree object for a given refinement level.
+   *
+   * This function initializes the ElementTree for an uniform refinement level.
+   * After that the point list is filled.
+   *
+   * \param geom The geometry object to construct a cluster tree on.
+   * \param M refinement level of the ElementTree.
+   */
   void init_ClusterTree(const Geometry& geom, int M) {
     element_tree_.init_ElementTree(geom, M);
     points_ = element_tree_.computeElementEnclosings();
@@ -43,19 +89,53 @@ class ClusterTree {
   //////////////////////////////////////////////////////////////////////////////
   /// getter
   //////////////////////////////////////////////////////////////////////////////
+  /**
+   * \brief Return reference to the ElementTree.
+   *
+   * \return Reference to the ElementTree.
+   */
   ElementTree& get_element_tree() { return element_tree_; }
+  /**
+   * \brief Return const reference to the ElementTree.
+   *
+   * \return Const Reference to the ElementTree.
+   */
   const ElementTree& get_element_tree() const { return element_tree_; }
+  /**
+   * \brief Return point list with coordinates.
+   *
+   * \return 3xN matrix with N number of points in the ElementTree.
+   */
   const Eigen::MatrixXd& get_points() const { return points_; }
+  /**
+   * \brief Return const reference to the Geometry.
+   *
+   * \return Const Reference to the PatchVector.
+   */
   const PatchVector& get_geometry() const {
     return element_tree_.get_geometry();
   }
+  /**
+   * \brief Return maximum level of refinement.
+   *
+   * \return Level of refinement.
+   */
   int get_max_level() const { return element_tree_.get_max_level(); }
+  /**
+   * \brief Return number of elements in the ElementTree.
+   *
+   * \return Number of elements.
+   */
   int get_number_of_elements() const {
     return element_tree_.get_number_of_elements();
   }
   //////////////////////////////////////////////////////////////////////////////
   /// member functions
   //////////////////////////////////////////////////////////////////////////////
+  /**
+   * \brief Check all patches at the mid points of the edges if the
+   * parametrization and normal vector match.
+   */
   void checkOrientation() {
     std::vector<std::array<int, 4>> edges = element_tree_.patchTopologyInfo();
     std::vector<Eigen::Vector2d> edge_midpoints = {
